97a9d4805d8a3c1ddd5e9e9ef09440218e6250fc
Quốc Việt <thaibinhquocviet2002@gmail.com>
2024-06-30 01:00:17 +0700

    update navigation sidebar

--- Frontend/src/components/ui/Sidebar.tsx
+++ Frontend/src/components/ui/Sidebar.tsx
@@ -7,8 +7,8 @@ import { useNavigate } from "react-router-dom";
 
 const data = [
   { link: "", label: "Contact", icon: IconAddressBook },
-  { link: "", label: "Billing", icon: IconReceipt2 },
-  { link: "", label: "Security", icon: IconFingerprint },
+  { link: "", label: "Add Contact", icon: IconReceipt2 },
+  { link: "", label: "Advanced Filtering", icon: IconFingerprint },
   { link: "", label: "SSH Keys", icon: IconKey },
   { link: "", label: "Databases", icon: IconDatabaseImport },
   { link: "", label: "Authentication", icon: Icon2fa },
