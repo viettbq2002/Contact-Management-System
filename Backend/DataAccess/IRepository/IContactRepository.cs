1d8a4f66bdf126529366a717c6f8475ac88838ad
Quốc Việt <thaibinhquocviet2002@gmail.com>
2024-06-29 18:01:07 +0700

    create contact repository

--- Backend/DataAccess/IRepository/IContactRepository.cs
+++ Backend/DataAccess/IRepository/IContactRepository.cs
@@ -2,11 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BussinessLogic.Models;
 
 namespace DataAccess.IRepository
 {
     public interface IContactRepository
     {
-        
+        Task CreateContactAsync(Contact contact);
+        Task UpdateContactAsync(Contact contact);
+        Task<List<Contact>> GetContactsAsync();
+        Task<Contact> GetContactByIdAsync(Guid id);
+
+        Task DeleteContactAsync(Contact contact);
+
+        Task<List<Contact>> GetContactsByUserIdAsync(Guid userId);
+
     }
 }
